b304fd8495ce66e27071bc4d55375f2159d79c9c
RFX <2403230138@cesunbc.edu.mx>
2026-07-31 11:47:03 -0700

    Delete Frontend directory

--- Frontend/scripts/logout.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("logout").addEventListener("click", (event) => {
-    event.preventDefault();
-
-    // Eliminar token del localStorage
-    localStorage.removeItem("token");
-
-    // Redirigir a la página de login
-    window.location.href = "/FRONTEND/index.html"; 
-});
